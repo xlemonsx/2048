64c78f4fad96a89077ac8dda1fcdb7c11a5157e8
xlemonsx <forestblah2011@gmail.com>
2014-03-26 23:39:21 -0700

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -4,7 +4,7 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.storageManager = new StorageManager;
   this.actuator       = new Actuator;
 
-  this.startTiles     = 2;
+  this.startTiles     = 1;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
@@ -69,15 +69,7 @@ GameManager.prototype.addStartTiles = function () {
   }
 };
 
-// Adds a tile in a random position
-GameManager.prototype.addRandomTile = function () {
-  if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
-    var tile = new Tile(this.grid.randomAvailableCell(), value);
 
-    this.grid.insertTile(tile);
-  }
-};
 
 // Sends the updated grid to the actuator
 GameManager.prototype.actuate = function () {
@@ -161,7 +153,7 @@ GameManager.prototype.move = function (direction) {
           var merged = new Tile(positions.next, tile.value * 2);
           merged.mergedFrom = [tile, next];
 
-          self.grid.insertTile(merged);
+          
           self.grid.removeTile(tile);
 
           // Converge the two tiles' positions
@@ -170,8 +162,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+         
+          
         } else {
           self.moveTile(tile, positions.farthest);
         }
@@ -184,7 +176,6 @@ GameManager.prototype.move = function (direction) {
   });
 
   if (moved) {
-    this.addRandomTile();
 
     if (!this.movesAvailable()) {
       this.over = true; // Game over!
